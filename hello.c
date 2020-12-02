fe84e7ebcb3d44205d89f9b3e65b35b1675d7c6c
ahnaf <63817873+asimahnaf@users.noreply.github.com>
2020-12-02 13:25:49 +0600

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,22 @@
+#include <stdio.h>   
+ int main() 
+ {   
+     int a, b, x;
+     printf("inter the mcb ");
+     scanf("%d %d", &a, &b);
+     if(a<b){
+         x = a;
+     }else{
+         x = b;
+     }
+     for(0; x>=1; --x){
+
+         if(a%x==0 && b%x==0){
+         printf("%d\n", x);
+         break;
+     }
+
+    }
+     
+
+ }   
