f791122686ee1dd6b9f50397aa762f8c2c868dfd
ahnaf <63817873+asimahnaf@users.noreply.github.com>
2020-12-09 01:29:37 +0600

    Update hello-05.c

--- hello-05.c
+++ hello-05.c
@@ -1,18 +1,23 @@
 #include <stdio.h> 
 double pi = 3.1416;
+double Area_of_a_circle(double r);
 
-  
  int main() 
  {   
      double r, area;
-     printf("Input your circle radius: \n");
+     printf("Input your circle radius: ");
      scanf("%lf", &r);
-     area = (pi*r*r);
-     printf("Area of a circle is %0.2lf\n", area);
+     double circle = Area_of_a_circle(r);
+     printf("Area of a circle is %0.2lf\n", circle);
 
 
      return 0; 
 
- }   
+ }  
+
+double Area_of_a_circle(double r){
+    double area = (pi*r*r);
+    return area;
+}
 
  
